f66c7b4073ff343a688a988a63dceff2a13fb2a8
Karthik Iyengar <karthikeyan.iyengar@gmail.com>
2016-09-08 22:21:21 +0530

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # teleshell
-Shell access to your box using a Telegram Bot
+
+Gain shell access to your machine using a Telegram Bot. 
+
+Some cases where this might be helpful:
+* You're in a double NAT environment and do not have access to your ISP's routers.
+* You're behind a firewall which blocks incoming traffic.
+* You have a dynamic IP address and don't want to set up Dynamic DNS
+
+## Quick Start
+
+* Generate a telegram bot authorization token by following the instructions [here](https://core.telegram.org/bots#6-botfather)
+* Rename the configuration template file (config.example.json) to config.json. Here are the properties that are supported
+
+| Property | Type | Description |
+|---|---|---|
+| `token`	| String | The authorization token for your telegram bot |
+| `whitelist` | Array(Number) | An array of whitelisted user ID (see below), from which commands will be run. |
+| `processTimeout` | Number | Number of milliseconds after which the spawned process will be killed (Optional) |
+
+* `npm install`
+* `node server.js`
+* Now go to Telegram and ping the bot that you've created. If the token has been set correctly, you should see loglines like so on your terminal: `Unauthorized command from user <NAME> (<USER ID>) [Thu Sep 08 2016 22:39:02 GMT+0530 (IST)]: ls`
+* Copy the User ID and add it to the whitelist and you are done. The bot will alert you if it receives messages from non-whitelisted IDs
+
+Suggestions, contributions, criticism and pull requests welcome.
---
+++ server.js
@@ -0,0 +1,76 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+fs.readFile('config.json', 'utf-8', function(err, data) {
+	if (err) {
+		console.log('Unable to open the configuration file. Is config.json present?');
+		process.exit(1);
+	}
+	try {
+		config = JSON.parse(data);
+	} catch (e) {
+		throw e;
+	}
+	serve(config);
+});
+
+function serve(config) {
+	var { token, whitelist, processTimeout } = config;
+	processTimeout = !isNaN(processTimeout) ? parseInt(processTimeout) : null; 
+	var bot = new TelegramBot(token, {
+		polling: true
+	});
+	
+	console.log(`Server Ready, Let's go!`);
+	
+	bot.on('text', function(msg) {
+		const { first_name, last_name, id} = msg.chat;
+		const isAuthorized = whitelist.indexOf(id) > -1;
+		const replyToSender = bot.sendMessage.bind(this, id);
+		console.log(`${isAuthorized ? 'Running authorized' : 'Unauthorized'} command from user ${first_name} ${last_name} (${id}) [${new Date()}]: ${msg.text}`);
+		
+		if (!isAuthorized) {
+			bot.sendMessage(id, '*You are unauthorized. This incident will be reported.*', {
+				parse_mode: 'Markdown'
+			});
+			whitelist.forEach(user => bot.sendMessage(user, `Unauthorized Access Attempt [${new Date()}]: ${JSON.stringify(msg)}`))
+		} else {
+			const exec = require('child_process').exec;
+			child = exec(msg.text);
+			var timer;
+			if (processTimeout) {
+				timer = setProcessTimer(child, replyToSender, msg.text, processTimeout);
+			}
+			child.stdout.on('data', data => {
+				if (timer !== undefined) {
+					clearTimeout(timer);
+					timer = setProcessTimer(child, replyToSender, msg.text, processTimeout);
+				}
+				bot.sendMessage(id, `${data}`)
+					.catch(err => bot.sendMessage(id, err.message))
+			});
+
+			child.stderr.on('data', data => {
+				bot.sendMessage(id, `*Error: ${data}*`, { parse_mode: 'Markdown'});
+				bot.sendMessage(id, `*Killing Process*`, { parse_mode: 'Markdown'});
+				child.stdin.pause();
+				child.kill();
+			});
+
+			child.on('close', code => {
+				clearTimeout(timer)
+				bot.sendMessage(id, `*Process exited with code ${code}*`, { parse_mode: 'Markdown'});
+			});
+		}
+	});
+}
+
+function setProcessTimer(child, replyToSender, processName, duration) {
+	const id =setTimeout(f => {
+		if (!child.killed) {
+			replyToSender(`*Killing process '${processName}' after ${duration}ms of inactivity*`, { parse_mode: 'Markdown'})
+			child.kill()
+		}
+	}, duration);
+	return id;
+};
