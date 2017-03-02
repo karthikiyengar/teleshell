d45ec6072e3c790b351f6807c1ff3aa393485341
Karthik Iyengar <karthikeyan.iyengar@gmail.com>
2017-03-02 20:44:02 +0530

    Removed non-standard syntax to support lower node versions

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const config = require('./config.json');
+let config;
+try {
+	config = require('./config.json');
+} catch (e) {
+	throw new Error('Unable to open the configuration file. Is config.json present?');
+}
+
 
 (function serve() {	
-	if (config !== undefined) {
-		var { token, whitelist, processTimeout } = config;
-	} else {
-		console.log('Unable to open the configuration file. Is config.json present?');
-	}
-	processTimeout = !isNaN(processTimeout) ? parseInt(processTimeout) : null; 
-	var bot = new TelegramBot(token, {
+	config.processTimeout = !isNaN(config.processTimeout) ? parseInt(config.processTimeout) : null; 
+	var bot = new TelegramBot(config.token, {
 		polling: true
 	});
 	
 	console.log(`Server Ready, Let's go!`);
 	
 	bot.on('text', function(msg) {
-		const { first_name, last_name, id} = msg.chat;
-		const isAuthorized = whitelist.indexOf(id) > -1;
+		const first_name = msg.chat.first_name;
+		const last_name = msg.chat.last_name;
+		const id = msg.chat.id;
+		const isAuthorized = config.whitelist.indexOf(id) > -1;
 		const replyToSender = bot.sendMessage.bind(this, id);
 		console.log(`${isAuthorized ? 'Running authorized' : 'Unauthorized'} command from user ${first_name} ${last_name} (${id}) [${new Date()}]: ${msg.text}`);
 		
@@ -25,18 +27,20 @@ const config = require('./config.json');
 			bot.sendMessage(id, '*You are unauthorized. This incident will be reported.*', {
 				parse_mode: 'Markdown'
 			});
-			whitelist.forEach(user => bot.sendMessage(user, `Unauthorized Access Attempt [${new Date()}]: ${JSON.stringify(msg)}`))
+			
+			// send a message to all whitelisted users regarding unauthorized attempt
+			config.whitelist.forEach(user => bot.sendMessage(user, `Unauthorized Access Attempt [${new Date()}]: ${JSON.stringify(msg)}`))
 		} else {
 			const exec = require('child_process').exec;
-			child = exec(msg.text);
+			const child = exec(msg.text);
 			var timer;
-			if (processTimeout) {
-				timer = setProcessTimer(child, replyToSender, msg.text, processTimeout);
+			if (config.processTimeout) {
+				timer = setProcessTimer(child, replyToSender, msg.text, config.processTimeout);
 			}
 			child.stdout.on('data', data => {
 				if (timer !== undefined) {
 					clearTimeout(timer);
-					timer = setProcessTimer(child, replyToSender, msg.text, processTimeout);
+					timer = setProcessTimer(child, replyToSender, msg.text, config.processTimeout);
 				}
 				bot.sendMessage(id, `${data}`)
 					.catch(err => bot.sendMessage(id, err.message))
@@ -58,7 +62,7 @@ const config = require('./config.json');
 })();
 
 function setProcessTimer(child, replyToSender, processName, duration) {
-	const id =setTimeout(f => {
+	const id = setTimeout(() => {
 		if (!child.killed) {
 			replyToSender(`*Killing process '${processName}' after ${duration}ms of inactivity*`, { parse_mode: 'Markdown'})
 			child.kill()
